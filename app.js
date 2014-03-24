1d17abc6710a768cf01016a097b3b9526a0d1ce4
Andy Zhang <zhangandy94@gmail.com>
2014-03-23 23:27:27 -0400

    Removed unnecessary stuff in app.js

--- app.js
+++ app.js
@@ -1,26 +1,9 @@
-/**
-   Copyright 2013 AlchemyAPI
+var express = require('express');
+var consolidate = require('consolidate');
+var graph = require('fbgraph');
 
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
-
-       http://www.apache.org/licenses/LICENSE-2.0
-
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
-   */
-
-
-   var express = require('express');
-   var consolidate = require('consolidate');
-   var graph = require('fbgraph');
-
-   var app = express();
-   var server = require('http').createServer(app);
+var app = express();
+var server = require('http').createServer(app);
 
 //Create the AlchemyAPI object
 var AlchemyAPI = require('./alchemyapi');
@@ -101,94 +84,14 @@ server.listen(port, function(){
 });
 
 
-var demo_text = 'Yesterday dumb Bob destroyed my fancy iPhone in beautiful Denver, Colorado. I guess I will have to head over to the Apple Store and buy a new one.';
-var demo_url = 'http://www.npr.org/2013/11/26/247336038/dont-stuff-the-turkey-and-other-tips-from-americas-test-kitchen';
-var demo_html = '<html><head><title>Python Demo | AlchemyAPI</title></head><body><h1>Did you know that AlchemyAPI works on HTML?</h1><p>Well, you do now.</p></body></html>';
-
-
-
 function example(req, res) {
-	var output = {};
+  var query = "SELECT message FROM status WHERE uid = me()";
 
-	//Start the analysis chain
-	sentiment(data);
-  keyword(data);
-  res.render("chart", data);
-}
+  graph.fql(query, function(err, response) {
+    console.log(response); // { data: [ { name: 'Ricky Bobby' } ] }
+    res.render('example', {
+      data:response['data']
+    });
+  });
 
-function keywords(output) {
-	alchemyapi.keywords('text', demo_text, { 'sentiment':1 }, function(response) {
-		output['keywords'] = { text:demo_text, response:JSON.stringify(response,null,4), results:response['keywords'] };
-	});
-  return;
-}
-
-function sentiment(output) {
-	alchemyapi.sentiment('html', demo_html, {}, function(response) {
-		output['sentiment'] = { html:demo_html, response:JSON.stringify(response,null,4), results:response['docSentiment'] };
-	});
-  return;
-}
-
-
-function author(req, res, output) {
-	alchemyapi.author('url', demo_url, {}, function(response) {
-		output['author'] = { url:demo_url, response:JSON.stringify(response,null,4), results:response };
-		language(req, res, output);
-	});
-}
-
-
-function language(req, res, output) {
-	alchemyapi.language('text', demo_text, {}, function(response) {
-		output['language'] = { text:demo_text, response:JSON.stringify(response,null,4), results:response };
-		title(req, res, output);
-	});
-}
-
-
-function title(req, res, output) {
-	alchemyapi.title('url', demo_url, {}, function(response) {
-		output['title'] = { url:demo_url, response:JSON.stringify(response,null,4), results:response };
-		relations(req, res, output);
-	});
-}
-
-
-function relations(req, res, output) {
-	alchemyapi.relations('text', demo_text, {}, function(response) {
-		output['relations'] = { text:demo_text, response:JSON.stringify(response,null,4), results:response['relations'] };
-		category(req, res, output);
-	});
-}
-
-
-function category(req, res, output) {
-	alchemyapi.category('text', demo_text, {}, function(response) {
-		output['category'] = { text:demo_text, response:JSON.stringify(response,null,4), results:response };
-		feeds(req, res, output);
-	});
-}
-
-
-function feeds(req, res, output) {
-	alchemyapi.feeds('url', demo_url, {}, function(response) {
-		output['feeds'] = { url:demo_url, response:JSON.stringify(response,null,4), results:response['feeds'] };
-		microformats(req, res, output);
-	});
-}
-
-
-function microformats(req, res, output) {
-	alchemyapi.microformats('url', demo_url, {}, function(response) {
-		output['microformats'] = { url:demo_url, response:JSON.stringify(response,null,4), results:response['microformats'] };
-
-		var query = "SELECT message FROM status WHERE uid = me()";
-
-		graph.fql(query, function(err, res) {
-		  console.log(res); // { data: [ { name: 'Ricky Bobby' } ] }
-		});
-
-		res.render('example',output);
-	});
 }
